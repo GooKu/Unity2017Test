3d956187787b676cffc3ca56ccb29fd83bc5a954
Goo <kuwoods@hotmail.com>
2017-09-10 07:55:41 +0800

    Increase move out test.

--- Assets/ActiveTest/ActiveTest.cs
+++ Assets/ActiveTest/ActiveTest.cs
@@ -13,6 +13,7 @@ public class ActiveTest : MonoBehaviour
     private UnityEngine.UI.Text resultText;
 
     private List<GameObject> activeBoxList = new List<GameObject>();
+    private Vector3 moveOutPosition = new Vector3(99999, 99999, 99999);
 
     private void Start()
     {
@@ -27,20 +28,17 @@ public class ActiveTest : MonoBehaviour
     {
         for (int i = 0; i < testNumber; i++)
         {
+            activeBoxList[i].transform.position = Vector3.zero;
             Renderer[] renders = activeBoxList[i].GetComponentsInChildren<Renderer>();
             for (int j = 0; j < renders.Length; j++)
-            {
-                if(!renders[j].enabled)
-                    renders[j].enabled = true;
-            }
+                renders[j].enabled = true;
         }
 
         Stopwatch sw = new Stopwatch();
         sw.Start();
         for (int i = 0; i < testNumber; i++)
-        {
             activeBoxList[i].SetActive(isActive);
-        }
+
         sw.Stop();
 		showResult($"ActiveGameobject, active:${isActive}, result:{sw.ElapsedMilliseconds}ms");
     }
@@ -49,8 +47,8 @@ public class ActiveTest : MonoBehaviour
     {
         for (int i = 0; i < testNumber; i++)
         {
-            if (!activeBoxList[i].activeSelf)
-                activeBoxList[i].SetActive(true);
+            activeBoxList[i].transform.position = Vector3.zero;
+            activeBoxList[i].SetActive(true);
         }
 
         Stopwatch sw = new Stopwatch();
@@ -59,12 +57,31 @@ public class ActiveTest : MonoBehaviour
         {
             Renderer[] renders = activeBoxList[i].GetComponentsInChildren<Renderer>();
             for(int j = 0; j < renders.Length; j++)
-            {
                 renders[j].enabled = enable;
-            }
         }
         sw.Stop();
         showResult($"EnableRender, active:${enable}, result:{sw.ElapsedMilliseconds}ms");
+    }
+
+    public void OnMoveGameobjectTest(bool isMoveOut)
+    {
+        for (int i = 0; i < testNumber; i++)
+        {
+            activeBoxList[i].SetActive(true);
+            Renderer[] renders = activeBoxList[i].GetComponentsInChildren<Renderer>();
+            for (int j = 0; j < renders.Length; j++)
+                    renders[j].enabled = true;
+        }
+
+        Vector3 position = isMoveOut? moveOutPosition : Vector3.zero;
+
+        Stopwatch sw = new Stopwatch();
+        sw.Start();
+        for (int i = 0; i < testNumber; i++)
+            activeBoxList[i].transform.position = position;
+
+        sw.Stop();
+        showResult($"MoveGameobject, move out:${isMoveOut}, result:{sw.ElapsedMilliseconds}ms");
     }
 
     private void showResult(string result)
